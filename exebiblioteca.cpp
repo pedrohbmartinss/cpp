a34d81355d5498954644c85ea933e5ac87e04c91
Pedro Henrique Barbosa Martins <pedro.henriquecamargoo@gmail.com>
2024-08-30 15:35:25 -0300

    exe de cadastro de biblioteca

--- exebiblioteca.cpp
+++ exebiblioteca.cpp
@@ -9,15 +9,15 @@ struct livro{
 };
 
 livro cadastralivro(){
-    livro novolivro;
 
-    cout << "Qual o nome do livro? :";
+    livro novolivro;
+    cout << "Qual o nome do livro?: ";
     cin >> novolivro.titulo;
-    cout << "Qual o nome do autor? :";
+    cout << "Qual o nome do autor livro?: ";
     cin >> novolivro.autor;
-    cout << "Qual o ano de publicacao do livro? :";
+    cout << "Qual o ano de publicacao do livro?: ";
     cin >> novolivro.anopub;
-    cout << "Qual o valor do livro? :";
+    cout << "Qual o preco do livro?: ";
     cin >> novolivro.preco;
 
     return novolivro;
@@ -27,15 +27,15 @@ void exibelivro(livro livro){
     cout << "O nome do livro e: " << livro.titulo << endl;
     cout << "O nome do autor do livro e: " << livro.autor << endl;
     cout << "O ano de publicacao do livro e: " << livro.anopub << endl;
-    cout << "O valor do livro e: " << livro.preco << endl;
+    cout << "O preco do livro e: " << livro.preco << endl;
 }
 
 int main(){
     livro meulivro;
 
-    meulivro = cadastralivro();
-    
-    exibelivro(meulivro);
+    meulivro=cadastralivro();
 
+    exibelivro(meulivro);
     return 0;
+
 }
