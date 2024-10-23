c0f744a1d254508f927694fb8f6829b65f2462b9
Pedro Henrique Barbosa Martins <pedro.henriquecamargoo@gmail.com>
2024-10-23 15:38:17 -0300

    exe de fatorial

---
+++ estudo/fatorial.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+int fatorial(int n){
+    
+    if(n == 0){
+        n = 1;
+        return 1;
+    }
+    
+    if (n > 0){
+        n = n* fatorial(n-1);
+        return n;
+    } 
+    return n;
+}
+
+int main(){
+    int n;
+    cout << "Digite um numero inteiro positivo" << endl;
+    cin >> n;
+    
+    if (n < 0){
+        cout << "Numero invalido, escolha outro numero" << endl;
+        return 1;
+    }
+    
+    
+    cout << "O fatorial de " << n << " e: " << fatorial(n) << endl;
+    return 0;
+}
