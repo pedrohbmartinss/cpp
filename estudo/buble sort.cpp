ad2d660677391d0231cc2baf72adfd2653c284ba
Pedro Henrique Barbosa Martins <pedro.henriquecamargoo@gmail.com>
2024-12-05 13:44:39 -0300

    add exe de sort

---
+++ estudo/buble sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void ordenando (vector<int> &vetor, int n){
+    for (int i =0; i < n - 1; i++){
+        for (int j=0; j < n - 1; j++){
+            if ( vetor[j] > vetor[j + 1]){
+                swap(vetor[j], vetor[j + 1]);
+            }
+        }
+    }
+}
+
+int main(){
+    vector <int> vetor  = {34, 5, 7, 2, 9, 50, 32, 78};
+    int n = vetor.size();
+    
+    ordenando(vetor, n);
+    
+    for (int i=0; i < n-1;i++){
+        cout << vetor[i] << endl;
+    }
+    
+
+    return 0;
+}
